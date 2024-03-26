a04d47742a1f94a64f6267cca30e4f1e4724145e
ThomasASW <125454436+ThomasASW@users.noreply.github.com>
2024-03-26 21:44:30 +0530

    Added API for editing and multi export of events. Fix filter bug in getEvents API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const ics = require("ics");
 const dateFns = require("date-fns");
-const { writeFileSync, writeFile } = require("fs");
+const { writeFile } = require("fs");
 
 const client = new MongoClient("mongodb://localhost:27017");
 
@@ -23,7 +23,66 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/calendar/events", getEvents);
 app.post("/api/calendar/events", createNewEvent);
+app.post("/api/calendar/events/:id", editEvent);
 app.get("/api/calendar/events/export", getCalendarExport);
+app.get("/api/calendar/events/export/all", exportMultiple);
+
+async function exportMultiple(req, res) {
+  console.log("export multiple event called");
+  const db = client.db("calendar");
+  const cursor = db.collection("events").find({
+    startDateTime: {
+      $gte: req.query.startDateTime,
+    },
+    endDateTime: {
+      $lte: req.query.endDateTime,
+    },
+  });
+  let events = [];
+  for await (const doc of cursor) {
+    events.push(doc);
+  }
+  let eventsToBeExported = [];
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    let start = new Date(event.start);
+    let end = new Date(event.end);
+    event.start = [
+      start.getFullYear(),
+      start.getMonth(),
+      start.getDate(),
+      start.getHours(),
+      start.getMinutes(),
+    ];
+    event.end = [
+      end.getFullYear(),
+      end.getMonth(),
+      end.getDate(),
+      end.getHours(),
+      end.getMinutes(),
+    ];
+    delete event._id;
+    console.log(event);
+    eventsToBeExported.push(event);
+  }
+  console.log(eventsToBeExported);
+  let fileName = `${__dirname}/temp/events.ics`;
+  ics.createEvents(eventsToBeExported, (error, value) => {
+    if (error != null || error != undefined) {
+      console.log(error);
+      res.status(500).json({ error: "Server error" });
+    } else {
+      writeFile(fileName, value, (err) => {
+        if (err != null) {
+          console.log(err);
+        } else {
+          console.log("file saved successfully");
+          res.download(fileName, "events.ics");
+        }
+      });
+    }
+  });
+}
 
 async function getCalendarExport(req, res) {
   console.log("export event called");
@@ -75,6 +134,25 @@ async function createNewEvent(req, res) {
   res.json(event);
 }
 
+async function editEvent(req, res) {
+  console.log("edit event called");
+  console.log(req.params.id);
+  const db = client.db("calendar");
+  const event = await db.collection("events").updateOne(
+    {
+      _id: new ObjectId(`${req.params.id}`),
+    },
+    {
+      $set: req.body,
+    },
+    {
+      upsert: false,
+    }
+  );
+  console.log(event);
+  res.json(event);
+}
+
 async function getEvents(req, res) {
   console.log("get events called");
   console.log(req.query);
@@ -82,10 +160,8 @@ async function getEvents(req, res) {
   const cursor = db.collection("events").find({
     startDateTime: {
       $gte: req.query.startDateTime,
-      $lte: req.query.endDateTime,
     },
     endDateTime: {
-      $gte: req.query.startDateTime,
       $lte: req.query.endDateTime,
     },
   });
